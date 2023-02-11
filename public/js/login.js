483a6f6e6d01157594c4216b70e6ffc71ff0f7a2
NEO <qj82189@gmail.com>
2023-02-11 11:56:08 -0500

    got signup to work

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/');
       } else {
-        alert('Failed to log in');
+        alert(response.statusText);
       }
     }
   };
@@ -23,10 +23,30 @@ const loginFormHandler = async (event) => {
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
 
-function takeToLogin() {
-  
 
-  document.location.replace('/login');
-}
+const signupFormHandler = async (event) => {
+  event.preventDefault();
 
-document.querySelector('#login').addEventListener('click', takeToLogin)
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('#signup-form')
+  .addEventListener('submit', signupFormHandler);
+
