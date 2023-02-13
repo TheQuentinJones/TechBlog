11cfcf2d2d28d0b53562a5ef59e7b0832f6e3342
NEO <qj82189@gmail.com>
2023-02-13 15:19:38 -0500

    make it better

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,10 +69,20 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/create', withAuth, (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.render('create');
+    return;
+  }
+
+  res.render('login');
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.render('homepage');
     return;
   }
 
